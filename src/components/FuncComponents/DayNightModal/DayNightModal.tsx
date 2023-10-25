ea337f30a888ee697fc26f82b6da504418b8d3f4
Igori <igor.buga@feel-it-services.com>
2023-10-25 17:52:11 +0300

    modal was added

--- src/components/FuncComponents/DayNightModal/DayNightModal.tsx
+++ src/components/FuncComponents/DayNightModal/DayNightModal.tsx
@@ -1,1 +1,37 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const DayNightModal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  return (
+    <Modal open={isOpen} onClose={onClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          borderRadius: '10px',
+          top: '60%',
+          left: '45%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          height: 300,
+          bgcolor: 'background.paper',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        {children}
+        <Button onClick={onClose}>Close</Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default DayNightModal;
